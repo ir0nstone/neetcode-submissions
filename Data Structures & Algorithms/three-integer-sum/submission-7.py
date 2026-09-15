b05d9a1ab13d705f9fbf3879f87bf6780b78d870
Andrej L <37474608+ir0nstone@users.noreply.github.com>
2026-09-15 16:45:22 +0100

    Add: three-integer-sum - submission-7

---
+++ Data Structures & Algorithms/three-integer-sum/submission-7.py
@@ -0,0 +1,29 @@
+class Solution:
+    def twoSum(self, numbers: list[int], target: int) -> list[int]:
+        results = {}
+
+        valid = []
+
+        for i, n in enumerate(numbers):
+            if n in results:
+                valid.append([results[n], i])
+
+            results[target-n] = i
+        
+        return valid
+
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+
+        seen = set()
+        vals = []
+
+        for idx, num in enumerate(nums):
+            for left, right in self.twoSum(nums[idx+1:], -num):
+                triplet = (num, nums[left+idx+1], nums[right+idx+1])
+                
+                if triplet not in seen:
+                    vals.append(list(triplet))
+                    seen.add(triplet)
+        
+        return vals
